d2558a599cd76b42bab2fd6e26fa8b121a3ca90b
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-04 18:20:04 +0300

    added Verlet integr scheme

--- spring/integr.cs
+++ spring/integr.cs
@@ -1,12 +1,22 @@
-﻿namespace spring
+﻿using System;
+
+namespace spring
 {
     public class Integr
     {
-        public static void EulerExpl(ref float[][] now, float[][] before, float dt, float m,int ff)
+        private const float hlf = 1 / 2;
+        public static void EulerExpl(ref float[][] now, float[][] before, float dt, float m)
         {
             now[(int)N.a][(int)C.x] = before[(int)N.f][(int)C.x] / m;
             now[(int)N.v][(int)C.x] = before[(int)N.v][(int)C.x] + before[(int)N.a][(int)C.x] * dt;
             now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + before[(int)N.v][(int)C.x] * dt;
+            now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + before[(int)N.u][(int)C.x];
+        }
+        public static void Verlet(ref float[][] now, float[][] before, float dt, float m)
+        {
+            now[(int)N.a][(int)C.x] = before[(int)N.f][(int)C.x] / m;
+            now[(int)N.v][(int)C.x] = before[(int)N.v][(int)C.x] + (hlf * (before[(int)N.a][(int)C.x] + now[(int)N.a][(int)C.x]) * dt);
+            now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + before[(int)N.v][(int)C.x] * dt + (hlf * before[(int)N.a][(int)C.x] * (float)Math.Pow(dt, 2));
             now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + before[(int)N.u][(int)C.x];
         }
     }
