e80213659ac60a41a365729295cd6c523a4e5d4f
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-06-04 18:28:01 +0300

    added Euler Implicit

--- spring/integr.cs
+++ spring/integr.cs
@@ -12,11 +12,25 @@ namespace spring
             now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + before[(int)N.v][(int)C.x] * dt;
             now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + before[(int)N.u][(int)C.x];
         }
+        public static void EulerImpl(ref float[][] now, float[][] before, float dt, float m)
+        {
+            now[(int)N.a][(int)C.x] = now[(int)N.f][(int)C.x] / m;
+            now[(int)N.v][(int)C.x] = before[(int)N.v][(int)C.x] + now[(int)N.a][(int)C.x] * dt;
+            now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + now[(int)N.v][(int)C.x] * dt;
+            now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + now[(int)N.u][(int)C.x];
+        }
         public static void Verlet(ref float[][] now, float[][] before, float dt, float m)
         {
             now[(int)N.a][(int)C.x] = before[(int)N.f][(int)C.x] / m;
             now[(int)N.v][(int)C.x] = before[(int)N.v][(int)C.x] + (hlf * (before[(int)N.a][(int)C.x] + now[(int)N.a][(int)C.x]) * dt);
             now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + before[(int)N.v][(int)C.x] * dt + (hlf * before[(int)N.a][(int)C.x] * (float)Math.Pow(dt, 2));
+            now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + before[(int)N.u][(int)C.x];
+        }
+        public static void Gear(ref float[][] now, float[][] before, float dt, float m)
+        {
+            now[(int)N.a][(int)C.x] = before[(int)N.f][(int)C.x] / m;
+            now[(int)N.v][(int)C.x] = before[(int)N.v][(int)C.x] + before[(int)N.a][(int)C.x] * dt;
+            now[(int)N.u][(int)C.x] = before[(int)N.u][(int)C.x] + before[(int)N.v][(int)C.x] * dt;
             now[(int)N.p][(int)C.x] = before[(int)N.p][(int)C.x] + before[(int)N.u][(int)C.x];
         }
     }
