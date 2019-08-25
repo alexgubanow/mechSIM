ff9f57e00e78d6942132fd820f25c15096ded4f7
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-08-25 13:12:04 +0300

    added unit test proj and one test to tryout

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ad7a1b3-8017-49b5-8066-d60b05b0e0b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using spring;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace spring.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void GetDCMTest()
+        {
+            float[] dcm = crds.GetDCM(new float[3] { 1,0,0 }, new float[3] { 0, 0, 1 });
+            CollectionAssert.AreEqual(new float[9] { 1, 0, 0, 0, 0, 1, 0, -1, 0 }, dcm);
+        }
+    }
+}
