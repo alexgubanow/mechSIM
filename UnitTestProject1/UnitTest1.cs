1ac407c58860511d1b9cbaf46c7b939900e13a21
Oleksandr Hubanov <alexgubanow@ukr.net>
2019-08-25 14:48:34 +0300

    added more test of DCM

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,10 +8,22 @@ namespace spring.Tests
     public class UnitTest1
     {
         [TestMethod()]
-        public void GetDCMTest()
+        public void GetDCMTest1()
         {
-            float[] dcm = crds.GetDCM(new float[3] { 1,0,0 }, new float[3] { 0, 0, 1 });
+            float[] dcm = crds.GetDCM(new float[3] { 1, 0, 0 }, new float[3] { 0, 0, 1 });
             CollectionAssert.AreEqual(new float[9] { 1, 0, 0, 0, 0, 1, 0, -1, 0 }, dcm);
+        }
+        [TestMethod()]
+        public void GetDCMTest2()
+        {
+            float[] dcm = crds.GetDCM(new float[3] { 0, 1, 0 }, new float[3] { 0, 0, 1 });
+            CollectionAssert.AreEqual(new float[9] { 0, 1, 0, 0, 0, 1, 1, 0, 0 }, dcm);
+        }
+        [TestMethod()]
+        public void GetDCMTest3()
+        {
+            float[] dcm = crds.GetDCM(new float[3] { 0, 0, 1 }, new float[3] { 0, 0, 1 });
+            CollectionAssert.AreEqual(new float[9] { 0, 0, 1, 0, 0, 1, 0, 0, 0 }, dcm);
         }
     }
 }
