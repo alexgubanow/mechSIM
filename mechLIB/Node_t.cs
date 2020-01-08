306990ed25086e8389850ce9542d6da8995d5241
Oleksandr Hubanov <alexgubanow@ukr.net>
2020-01-09 00:14:30 +0200

    FIXED gravity force

--- mechLIB/Node_t.cs
+++ mechLIB/Node_t.cs
@@ -29,7 +29,8 @@ namespace mechLIB
                 deriv[i] = new deriv_t();
             }
             deriv[0].p = coords;
-            //deriv[0].a = new xyz_t() { x = 0, y = maf._g, z = 0 };
+            deriv[0].a = new xyz_t() { x = 0, y = maf._g, z = 0 };
+            deriv[0].v = new xyz_t() { x = 0, y = maf._g * 5E-06f, z = 0 };
             radiusPoint = _radiusPoint;
         }
         public void CalcAccel(int t, float m)
@@ -44,9 +45,9 @@ namespace mechLIB
         }
         public void GetForces(ref Rope_t model, int t, float m, float c)
         {
-            xyz_t Fg = new xyz_t();
-            Fg.y = m * maf._g;
-            F[t].Plus(Fg);
+            //xyz_t Fg = new xyz_t();
+            //Fg.y = m * maf._g;
+            //F[t].Plus(Fg);
 
             xyz_t Fd = new xyz_t();
             Fd.x = 0 - (c * deriv[t - 1].v.x);
@@ -54,19 +55,19 @@ namespace mechLIB
             Fd.z = 0 - (c * deriv[t - 1].v.z);
             F[t].Plus(Fd);
             /*getting element forces*/
-            //foreach (var neigNode in Neigs)
-            //{
-            //    //getting position of link according base point
-            //    xyz_t LinkPos = new xyz_t();
-            //    LinkPos.Minus(deriv[t - 1].p, model.GetNodeRef(neigNode).deriv[t - 1].p);
-            //    //getting DCM for this link
-            //    dcm_t dcm = new dcm_t(LinkPos, radiusPoint);
-            //    xyz_t gFn = new xyz_t();
-            //    //convert Fn to global coords and return
-            //    dcm.ToGlob(model.GetElemRef(ID, neigNode).F[t], ref gFn);
-            //    //push it to this force pull
-            //    F[t].Plus(gFn);
-            //}
+            foreach (var neigNode in Neigs)
+            {
+                //getting position of link according base point
+                xyz_t LinkPos = new xyz_t();
+                LinkPos.Minus(deriv[t - 1].p, model.GetNodeRef(neigNode).deriv[t - 1].p);
+                //getting DCM for this link
+                dcm_t dcm = new dcm_t(LinkPos, radiusPoint);
+                xyz_t gFn = new xyz_t();
+                //convert Fn to global coords and return
+                dcm.ToGlob(model.GetElemRef(ID, neigNode).F[t], ref gFn);
+                //push it to this force pull
+                F[t].Plus(gFn);
+            }
         }
         public void GetPhysicParam(ref Rope_t rope, int t, float Re, ref float m, ref float c)
         {
@@ -78,6 +79,7 @@ namespace mechLIB
                 c += cElem;
                 m += mElem;
             }
+            //m = 1E-04f;
             c /= Neigs.Length;
             if (m <= 0)
             {
