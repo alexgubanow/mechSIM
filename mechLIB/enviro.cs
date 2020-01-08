c220a1c970e0e951607e14e4785a0b987bee4e9b
Oleksandr Hubanov <alexgubanow@ukr.net>
2020-01-09 00:14:57 +0200

    move to reading load from mat file

--- mechLIB/enviro.cs
+++ mechLIB/enviro.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using csmatio.io;
+using csmatio.types;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -34,44 +36,16 @@ namespace mechLIB
 
         private void ReadLoadFile(string loadFile)
         {
-            List<string> loadStrs = new List<string>();
-            using (StreamReader sr = File.OpenText(loadFile))
-            {
-                string line = string.Empty;
-                //read load back
-                while ((line = sr.ReadLine()) != null)
-                {
-                    loadStrs.Add(line);
-                }
-            }
-            float[] time = new float[loadStrs.Count];
-            float[] pmx = new float[loadStrs.Count];
-            float[] plx = new float[loadStrs.Count];
-            float[] pmy = new float[loadStrs.Count];
-            float[] ply = new float[loadStrs.Count];
-            //t	PMx	PMy	PLx	PLy
-            for (int i = 0; i < loadStrs.Count; i++)
-            {
-                string[] tmp = loadStrs[i].Split('\t');
-                time[i] = Convert.ToSingle(tmp[0]);
-                pmx[i] = Convert.ToSingle(tmp[1]);
-                plx[i] = Convert.ToSingle(tmp[2]);
-                pmy[i] = Convert.ToSingle(tmp[3]);
-                ply[i] = Convert.ToSingle(tmp[4]);
-            }
-            //interpolate
-            int ipCounts = (int)(loadStrs.Count * (time[1] - time[0]) / phProps.dt);
-            phProps.Counts = ipCounts;
-            allocateTime(phProps.dt, ipCounts);
-            float[] pmxip = new float[ipCounts];
-            float[] plxip = new float[ipCounts];
-            float[] pmyip = new float[ipCounts];
-            float[] plyip = new float[ipCounts];
-            int ratio = (int)((time[1] - time[0]) / phProps.dt);
-            maf.Linip(pmx, ratio, ref pmxip);
-            maf.Linip(plx, ratio, ref plxip);
-            maf.Linip(pmy, ratio, ref pmyip);
-            maf.Linip(ply, ratio, ref plyip);
+
+            MatFileReader mfr = new MatFileReader(loadFile);
+            // now get the double values 
+            time = (mfr.Content["tq"] as MLSingle).GetArray()[0];
+            phProps.Counts = time.Length;
+            float[] pmx = (mfr.Content["pmxq"] as MLSingle).GetArray()[0];
+            float[] plx = (mfr.Content["plxq"] as MLSingle).GetArray()[0];
+            float[] pmy = (mfr.Content["pmyq"] as MLSingle).GetArray()[0];
+            float[] ply = (mfr.Content["plyq"] as MLSingle).GetArray()[0];
+
             rope = new Rope_t(phProps);
             //fill load to rope
             Re = new float[phProps.Counts];
@@ -82,12 +56,12 @@ namespace mechLIB
                 Re[t] = 0;
                 bloodV[t] = 0;
                 bloodP[t] = 0;
-                rope.Nodes[0].deriv[t].p.x = rope.Nodes[0].deriv[0].p.x + pmxip[t];
-                rope.Nodes[0].deriv[t].p.y = rope.Nodes[0].deriv[0].p.y + pmyip[t];
+                rope.Nodes[0].deriv[t].p.x = rope.Nodes[0].deriv[0].p.x + pmx[t];
+                rope.Nodes[0].deriv[t].p.y = rope.Nodes[0].deriv[0].p.y + pmy[t];
 
                 int lastN = phProps.nodes - 1;
-                rope.Nodes[lastN].deriv[t].p.x = rope.Nodes[lastN].deriv[0].p.x + plxip[t];
-                rope.Nodes[lastN].deriv[t].p.y = rope.Nodes[lastN].deriv[0].p.y + plyip[t];
+                rope.Nodes[lastN].deriv[t].p.x = rope.Nodes[lastN].deriv[0].p.x + plx[t];
+                rope.Nodes[lastN].deriv[t].p.y = rope.Nodes[lastN].deriv[0].p.y + ply[t];
             }
             //choose load nodes
             rope.Nodes[0].LoadType = NodeLoad.p;
@@ -110,25 +84,27 @@ namespace mechLIB
             float A = (float)Math.PI * (float)Math.Pow(phProps.D, 2) / 4;
             float maxLoad = ((phProps.E * A) / phProps.L / phProps.nodes) * phProps.MaxU;
             float freq = 1 / (phProps.Counts * phProps.dt);
-            rope.Nodes[0].LoadType = NodeLoad.f;
+            rope.Nodes[0].LoadType = NodeLoad.p;
             rope.Nodes[phProps.nodes - 1].LoadType = NodeLoad.p;
-            //for (int t = 0; t < phProps.Counts; t++)
-            //{
-            //    Re[t] = 0;
-            //    bloodV[t] = 0;
-            //    bloodP[t] = 0;
-            //    rope.Nodes[0].F[t].x = 0 - ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad);
-            //    //model.Nodes[0].deriv[t].p.z = model.Nodes[0].deriv[0].p.z;
-            //    //model.Nodes[0].deriv[t].p.y = model.Nodes[0].deriv[0].p.y;
-            //    //model.Nodes[0].deriv[t].p.x = 0 - ((time[t] + time[1]) * phProps.MaxU);
-            //    //model.Nodes[0].deriv[t].v.x = (model.Nodes[0].deriv[t].p.x - (0 - (time[t] * phProps.MaxU))) / time[1];
-            //    int lastN = phProps.nodes - 1;
-            //    rope.Nodes[lastN].deriv[t].p.z = rope.Nodes[lastN].deriv[0].p.z;
-            //    rope.Nodes[lastN].deriv[t].p.y = rope.Nodes[lastN].deriv[0].p.y;
-            //    rope.Nodes[lastN].deriv[t].p.x = rope.Nodes[lastN].deriv[0].p.x;
-            //    //model.Nodes[lastN].F[t].x = ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad);
-            //    //model.Nodes[lastN].deriv[t].p.x =  ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad) + model.Nodes[lastN].deriv[0].p.x;
-            //}
+            for (int t = 0; t < phProps.Counts; t++)
+            {
+                rope.Nodes[0].deriv[t].p = rope.Nodes[0].deriv[0].p;
+                rope.Nodes[phProps.nodes - 1].deriv[t].p = rope.Nodes[phProps.nodes - 1].deriv[0].p;
+                //    Re[t] = 0;
+                //    bloodV[t] = 0;
+                //    bloodP[t] = 0;
+                //    rope.Nodes[0].F[t].x = 0 - ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad);
+                //    //model.Nodes[0].deriv[t].p.z = model.Nodes[0].deriv[0].p.z;
+                //    //model.Nodes[0].deriv[t].p.y = model.Nodes[0].deriv[0].p.y;
+                //    //model.Nodes[0].deriv[t].p.x = 0 - ((time[t] + time[1]) * phProps.MaxU);
+                //    //model.Nodes[0].deriv[t].v.x = (model.Nodes[0].deriv[t].p.x - (0 - (time[t] * phProps.MaxU))) / time[1];
+                //    int lastN = phProps.nodes - 1;
+                //    rope.Nodes[lastN].deriv[t].p.z = rope.Nodes[lastN].deriv[0].p.z;
+                //    rope.Nodes[lastN].deriv[t].p.y = rope.Nodes[lastN].deriv[0].p.y;
+                //    rope.Nodes[lastN].deriv[t].p.x = rope.Nodes[lastN].deriv[0].p.x;
+                //    //model.Nodes[lastN].F[t].x = ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad);
+                //    //model.Nodes[lastN].deriv[t].p.x =  ((float)Math.Sin(2 * Math.PI * 0.5 * time[t] * freq) * maxLoad) + model.Nodes[lastN].deriv[0].p.x;
+            }
         }
         public void Run()
         {
